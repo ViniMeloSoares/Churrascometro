de4e80e53a3aebbfc0eca2bbbfa59e61c156c95a
ViniMeloSoares <vinimelosoares@gmail.com>
2024-11-04 21:22:38 -0300

    melhorias layout pagina principal

---
+++ app/src/main/java/com/viniciusmelo/churrascmetro/Result_Screen.kt
@@ -0,0 +1,20 @@
+package com.viniciusmelo.churrascmetro
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Result_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_result_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
